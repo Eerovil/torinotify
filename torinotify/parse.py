751754868bf2a6c164ca3e76743d76418471099f
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-05-18 22:33:14 +0300

    Add title must contain

--- torinotify/parse.py
+++ torinotify/parse.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import re
 
 from bs4 import BeautifulSoup
 
@@ -30,33 +31,40 @@ def main():
         parsed = {}
 
     for entry in entries:
-        print("Fetching entry {}".format(entry['name']))
-        response = requests.get(entry['url'])
-        soup = BeautifulSoup(response.content, 'html.parser')
-        new_links = []
-        if entry['url'] not in parsed:
-            parsed[entry['url']] = []
+        try:
+            print("Fetching entry {}".format(entry['name']))
+            response = requests.get(entry['url'])
+            soup = BeautifulSoup(response.content, 'html.parser')
+            new_links = []
+            if entry['url'] not in parsed:
+                parsed[entry['url']] = []
 
-        for link in soup.select("div.main div.list_mode_thumb a"):
-            if not link.select_one('.desc_flex'):
-                continue
-            title = " ".join([
-                _part for _part in link.select_one('.ad-details-left').getText().split()
-            ])
-            link_parsed = link['href'].split('?')[0]
-            if parsed[entry['url']] and link_parsed == parsed[entry['url']][0]['url']:
-                break
-            else:
+            for link in soup.select("div.main div.list_mode_thumb a"):
+                if not link.select_one('.desc_flex'):
+                    continue
+                title = " ".join([
+                    _part for _part in link.select_one('.ad-details-left').getText().split()
+                ])
+                link_parsed = link['href'].split('?')[0]
+                if parsed[entry['url']] and link_parsed == parsed[entry['url']][0]['url']:
+                    break
+
+                if entry.get('title_must_contain', None):
+                    if not re.search(entry['title_must_contain'], title):
+                        continue
+
                 new_links.append({'title': title, 'url': link_parsed})
 
-        if entry['url'] not in parsed:
-            parsed[entry['url']] = []
-        parsed[entry['url']] = new_links + parsed[entry['url']]
-        parsed[entry['url']] = parsed[entry['url']][:10]
+            if entry['url'] not in parsed:
+                parsed[entry['url']] = []
+            parsed[entry['url']] = new_links + parsed[entry['url']]
+            parsed[entry['url']] = parsed[entry['url']][:10]
 
-        for new_link in new_links:
-            print("Found new links: {}".format(new_link))
-            send_telegram("{}, {}".format(new_link['title'], new_link['url']), entry['chatId'])
+            for new_link in new_links:
+                print("Found new links: {}".format(new_link))
+                send_telegram("{}, {}".format(new_link['title'], new_link['url']), entry['chatId'])
+        except Exception as e:
+            print("Error: {}".format(e))
 
     with open('parsed.json', 'w') as f:
         json.dump(parsed, f, indent=4)
